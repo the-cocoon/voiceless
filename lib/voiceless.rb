b4c337d253536a7871b4ef01332be99ea5fd265b
Ilya N. Zykin <zykin-ilya@ya.ru>
2017-03-11 18:46:18 +0300

    v11.03.2017

---
+++ lib/voiceless.rb
@@ -0,0 +1,26 @@
+require 'voiceless/version'
+require 'colorize'
+
+module Voiceless; end
+
+def voiceless
+  begin
+    yield
+  rescue Exception => e
+    puts (">"*50).red
+    puts "VOICELESS ALERT: #{ e.message.to_s }".red
+
+    line = caller.each_with_index.map do |item, index|
+      caller[index].split(':')[0..1].join(':')
+    end.compact.select do |item|
+      item unless item =~ /voiceless/mix
+    end.first.to_s
+
+    puts line.yellow if line.present?
+    puts ("<"*50).red
+  end
+end
+
+def voiceless_include
+  voiceless { include yield }
+end
---
+++ lib/voiceless/version.rb
@@ -0,0 +1,3 @@
+module Voiceless
+  VERSION = "1.0.0"
+end
